f22f733790f629c2a5b82750894d722d320d83a6
Nigini Abilio <nigini@gmail.com>
2014-09-07 20:57:30 -0300

    Finishing tutorial's part 2.

--- training/admin.py
+++ training/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 from training.models import Athlete,Activity
 
-admin.site.register(Athlete)
-admin.site.register(Activity)
+class ActivityInline(admin.TabularInline):
+    model = Activity
+    extra = 1
+
+class AthleteAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name']}),
+    ]
+    inlines = [ActivityInline]
+    list_display = ('name', 'last_registered_activity_date')
+    #list_filter = ['last_registered_activity_date']
+
+class ActivityAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields':['athlete']}),
+        ('When', {'fields': ['date']}),
+        ('What', {'fields': ['sport', 'description']})
+    ]
+
+admin.site.register(Athlete, AthleteAdmin)
+admin.site.register(Activity, ActivityAdmin)
--- training/models.py
+++ training/models.py
@@ -3,6 +3,14 @@ from django.db import models
 class Athlete(models.Model):
     name = models.CharField(max_length=200)
 
+    def last_registered_activity_date(self):
+        last_activity = Activity.objects.filter(athlete=self.id).order_by('-date').first()
+        if last_activity:
+            return last_activity.date
+        else:
+            return None
+    last_registered_activity_date.short_description = 'Date of last registered activity'
+
     def __str__(self):
         return self.name
 
@@ -13,4 +21,4 @@ class Activity(models.Model):
     date = models.DateField()
 
     def __str__(self):
-        return '{}: {}'.format(self.date,self.description)
+        return '{}: {}: {}'.format(self.athlete, self.date, self.description)
