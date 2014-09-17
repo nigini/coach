20ad0798a570bb2e7b4c4ad937426fc03d8c2b16
Nigini Abilio <nigini@gmail.com>
2014-09-16 21:48:27 -0300

    Implementation of a basic login system (issue #2).

--- coach/urls.py
+++ coach/urls.py
@@ -4,4 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^training/', include('training.urls', namespace='training')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'training/login.html'}, name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/training/team/'}, name='logout'),
 )
--- training/urls.py
+++ training/urls.py
@@ -3,7 +3,7 @@ from training import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^team/$', views.get_athletes_list, name='athletes_list'),
+    url(r'^team/$', views.get_athletes_list, name='team'),
     url(r'^athletes/(?P<athlete_id>\d+)/$', views.get_athlete, name='athlete'),
     url(r'^athletes/(?P<athlete_id>\d+)/feedback/(?P<activity_id>\d+)/$', views.feedback, name='feedback'),
 
